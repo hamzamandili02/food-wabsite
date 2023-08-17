5e65afdf512e3189a4c871e9991b16c95f191d90
hamzamandili02 <hamzamandili02@gmail.com>
2023-08-17 11:28:00 +0200

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'food-wabsite',
   plugins: [vue()],
-  // publicPath: process.env.NODE_ENV === "production" ? "/food-recipes/" : "/",
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/food-recipes/'
-    : '/'
-}
+
 
